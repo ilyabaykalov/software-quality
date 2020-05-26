eb7f8923ab6004a9e590dc4875d47dbb9cf3d796
Ilya Baykalov <enot_boris@icloud.com>
2020-05-26 22:27:18 +0700

    fourth lab

--- fourth-lab/src/test/java/org/ilyabaykalov/KernelStub.java
+++ fourth-lab/src/test/java/org/ilyabaykalov/KernelStub.java
@@ -5,31 +5,26 @@ import model.Order;
 
 import java.util.Observable;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-// This is a Kernel stub for the Ordering testing purposes
 public class KernelStub implements Kernel {
 	
 	private static final int COUNT = 1;
-	private CountDownLatch order_latch_ = new CountDownLatch(COUNT);
+	private CountDownLatch countDownLatch = new CountDownLatch(COUNT);
 	private Order order = new Order();
-	public final static Lock lock = new ReentrantLock();
-	
+
 	public Order GetOrder() {
 		return order;
 	}
 	
 	public void Reset() {
-		order_latch_ = new CountDownLatch(COUNT);
+		countDownLatch = new CountDownLatch(COUNT);
 		order = null;
 	}
 	
 	public void Await() {
 		try {
-			order_latch_.await();
+			countDownLatch.await();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -37,11 +32,11 @@ public class KernelStub implements Kernel {
 	@Override
 	public final void update(Observable event_source, Object data) {
 		order = (Order)data;
-		Free();
+		run();
 	}
 
-	public void Free() {
-		order_latch_.countDown();
+	public void run() {
+		countDownLatch.countDown();
 	}
 
 	@Override
